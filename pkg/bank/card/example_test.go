eee9529a89bcdbb93a52e217ceef028ddb1317ae
Bekmrod <beki031291@gmail.com>
2021-03-25 15:40:23 +0430

    добавлены тесты на отриц.баланс, неактивную карту и несколько карт для Total

--- pkg/bank/card/example_test.go
+++ pkg/bank/card/example_test.go
@@ -4,6 +4,45 @@ import (
 	"fmt"
 )
 func ExampleTotal(){
+	
+	fmt.Println(Total([]types.Card{
+		{
+			Balance: 100000,
+			Active: true,
+		},
+	}))
+	fmt.Println(Total([]types.Card{
+		{
+			Balance:100000,
+			Active:true,
+		},
+		{
+				Balance: 200000,
+				Active: true,
+			},
+	
+	}))
+	fmt.Println(Total([]types.Card{
+		{
+			Balance: 100000,
+			Active: false,
+		},
+
+	}))
+	fmt.Println(Total([]types.Card{
+		{
+			Balance: -100000,
+			Active: true,
+		},
+	}))
+	// Output:
+	//100000
+	//300000
+	//0
+	//0
+	
+}
+/*
 	cards := [] types.Card{
 	{
 		Balance:500,
@@ -23,4 +62,4 @@ func ExampleTotal(){
 
 	fmt.Println(Total(cards))
 //output: 500
-}
+*/
